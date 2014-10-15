316cf9467b307b198d8b1a2f4b0f45aa47a7aecd
garin <garinspring@163.com>
2014-10-15 17:07:17 +0800

    ok

--- watteNN/watteNN/DetailViewController.m
+++ watteNN/watteNN/DetailViewController.m
@@ -29,6 +29,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor grayColor];
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue]>=7.0) {
+        self.automaticallyAdjustsScrollViewInsets = NO;
+    }
+    
+    
+    UIScrollView *contentView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    contentView.delegate = self;
+    contentView.backgroundColor = [UIColor orangeColor];
+    contentView.contentSize = CGSizeMake(self.view.bounds.size.width,self.view.bounds.size.height+200);
+    //    contentView.bounces = YES;
+    //    contentView.alwaysBounceVertical = YES;
+    [self.view addSubview:contentView];
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    static float mark1 = 0;      //静态变量，记录contentOffset上次的位置
+    float offY = scrollView.contentOffset.y;   //本次contentOffset的位置
+    if (offY > mark1) {      //offY > mark1 表示现在是下滚状态
+        if (offY > 0) {        // 这个判断很关键，不信去掉试试
+            [((myTarBarViewController *)self.tabBarController) setTabBarHidden:YES];   //隐藏tabbar
+        }
+        
+    }
+    else              //offY < mark1 表示现在是上滚状态
+    {
+        if (offY < scrollView.contentSize.height - scrollView.frame.size.height) {  // 这个判断很关键，不信去掉试试
+            [((myTarBarViewController *)self.tabBarController) setTabBarHidden:NO];   //不隐藏tabbar
+        }
+        
+    }
+    mark1 = offY;    ///静态变量记录contentOffset本次的位置
 }
 
 - (void)didReceiveMemoryWarning
